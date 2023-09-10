bde7cd709f9e29b871abd32137ce50f3b192a581
633230019 <144575516+633230019@users.noreply.github.com>
2023-09-10 19:38:03 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title('My title')
+st.title('ทดสอบการเขียนเว็บด้วย Streamlit')
 col1, col2 = st.columns(2)
 # col1.write("This is column 1")
 # col2.write("This is column 2")
 with col1:
-    st.header("A")
-with col2:
+    st.header("Iris")
     st.image("./pic/iris1.jpg")
+with col2:
+    st.header
 
-st.header('สิทธิพงษ์')
+st.header('สิทธิพงษ์ แจ้งไพร')
 st.subheader('My sub')
