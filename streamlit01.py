0aa0ca8a4a573234daf3fbbda2ee2c0ff11afbb9
633230019 <144575516+633230019@users.noreply.github.com>
2023-09-10 19:28:27 +0700

    .commit

--- streamlit01.py
+++ streamlit01.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
 st.title('My title')
-st.header('My header')
+col1, col2 = st.columns(2)
+col1.write("This is column 1")
+col2.write("This is column 2")
+
+
+st.header('สิทธิพงษ์')
 st.subheader('My sub')
